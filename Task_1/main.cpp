7c766d881b4eb0e693ff4ba2d9bbee3e971b317b
HP-PROBOOK-G10\antco <antonio.kolesnikow@gmail.com>
2024-11-26 22:44:41 +0300

    edited

--- Task_1/main.cpp
+++ Task_1/main.cpp
@@ -5,16 +5,21 @@
 int main() {
   setlocale(LC_ALL, "Russian");
   intro();
-  int k = correctInputk();
-  double **C = create2dArray(k);
-  double **D = create2dArray(k);
-  fillArrayC(C, k);
-  fillArrayD(D, k);
-  double SumC = countDiagSum(C, k);
-  double SumD = countDiagSum(D, k);
-  OutputInput(C, k, 'C', SumC);
-  OutputInput(D, k, 'D', SumD);
-  delete2dArray(C, k);
-  delete2dArray(D, k);
+  while (true) {
+    int k = correctInputk();
+    if (k == 0) {
+      break;
+    }
+    double **C = create2dArray(k);
+    double **D = create2dArray(k);
+    fillArrayC(C, k);
+    fillArrayD(D, k);
+    double SumC = countDiagSum(C, k);
+    double SumD = countDiagSum(D, k);
+    OutputInput(C, k, 'C', SumC);
+    OutputInput(D, k, 'D', SumD);
+    delete2dArray(C, k);
+    delete2dArray(D, k);
+  }
   return 0;
 }
